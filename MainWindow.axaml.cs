0bb8360ce19f7d7a521c037671a131806f7c1ef1
COMPLEXDEV <github.com yoshidatadao018@gmail.com>
2023-06-09 19:36:52 +0200

    First Commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+
+namespace RPN_Calculator_UI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
